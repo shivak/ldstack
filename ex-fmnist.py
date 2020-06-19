6597c05898cef1c43d2a4b319e9e86aa00524ab4
Shiva Kaul <skkaul@matrix.ml.cmu.edu>
2020-06-19 14:50:03 -0400

    Updated experiments.

--- ex-fmnist.py
+++ ex-fmnist.py
@@ -1,142 +1,83 @@
 import numpy as np
 import tensorflow as tf
-from ldstack import ldstack
-import sys
-sys.path.append('.')
-from cocob_optimizer import COCOB
+from ldstack import LDStack
 
-from tensorflow.examples.tutorials.mnist import input_data
-mnist = input_data.read_data_sets('tmp/fashion/', source_url='http://fashion-mnist.s3-website.eu-central-1.amazonaws.com/', one_hot=True)
-
-learning_rate = 0.1
-training_steps = 60000
-batch_size = 32
-display_every = 500
-total_batch = int(mnist.train.num_examples / batch_size)
-print("Total number of batches:", total_batch)
+learning_rate = 0.001
+batch_size = 64
+num_epochs = 30
 
 # Network Parameters
-num_input = 1 # MNIST data input (img shape: 28*28)
-timesteps = 28 * 28 # timesteps
-n_lstm = 32
-n_gru = 37
-n_lds = 32
-num_classes = 10 # MNIST total classes (0-9 digits)
-k = 16
-Del = 1
-num_layers = 1
+num_input = 1 
+timesteps = 28 * 28 
+num_hidden = 128 
+num_classes = 10 
+k = 1
+Delta = 2
 
-# Doesn't work on CUDNN RNNs because they use an opaque buffer for weights
-# Use count_params() instead
-def num_parameters(scope):
-  total_parameters = 0
-  for variable in tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope=scope):
-      # shape is an array of tf.Dimension
-      shape = variable.get_shape()
-      print(variable, shape)
-      variable_parameters = 1
-      for dim in shape:
-          variable_parameters *= dim.value
-      total_parameters += variable_parameters
-  return total_parameters
+tf.enable_eager_execution()
+tf.enable_v2_behavior()
 
-def lds(X, Y, scope):
-  with tf.variable_scope(scope):
-    outs = X
-    for i in np.arange(num_layers):
-      outs = tf.complex(outs, 0.0)
-      outs, (lnlam, _, _, _), (laminit, _, _, _) = ldstack(outs, n_lds, num_classes, (k if i > 0 else k), Del, scope + str(i), standard=True)
-    outs = outs[-1]
-    print(scope + " parameters: ", num_parameters(scope))
-    return trainer(outs, Y, scope, tf.train.AdamOptimizer(0.0001)), (lnlam, laminit)
-def gru(X, Y, scope):
-  with tf.variable_scope(scope):
-    g = tf.contrib.cudnn_rnn.CudnnGRU(1, n_gru, kernel_initializer=tf.orthogonal_initializer())
-    outs, _ = g(X)
-    outs = outs[-1]
-    outs = tf.layers.Dense(num_classes)(outs)
-    return trainer(outs, Y, scope, tf.train.AdamOptimizer(0.0001)), g
-def lstm(X, Y, scope):
-  with tf.variable_scope(scope):
-    g = tf.contrib.cudnn_rnn.CudnnLSTM(1, n_lstm, kernel_initializer=tf.orthogonal_initializer())
-    outs, _ = g(X)
-    outs = outs[-1]
-    outs = tf.layers.Dense(num_classes)(outs)
-    return trainer(outs, Y, scope, tf.train.AdamOptimizer(0.0001)), g    
+(x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
+train_dataset = tf.data.Dataset.from_tensor_slices(
+  (tf.cast(x_train.reshape(-1, timesteps, 1)/255, tf.float32),
+   tf.cast(y_train,tf.int64)))
+train_dataset = train_dataset.shuffle(3000).batch(batch_size)
+test_dataset = tf.data.Dataset.from_tensor_slices(
+  (tf.cast(x_test.reshape(-1, timesteps, 1)/255, tf.float32),
+   tf.cast(y_test,tf.int64)))
+test_dataset = test_dataset.batch(batch_size)
 
-def trainer(logits, Y, scope, optimizer):
+
+#lds = LDStack(num_hidden, num_input, num_classes, k, Delta)
+
+lds = tf.keras.Sequential([
+  tf.keras.layers.CuDNNGRU(num_hidden, kernel_initializer=tf.keras.initializers.Orthogonal()),
+  tf.keras.layers.BatchNormalization(),
+  tf.keras.layers.Dense(num_classes, kernel_initializer=tf.keras.initializers.Orthogonal())                           
+])
+
+def loss_accuracy(logits, Y):
   prediction = tf.nn.softmax(logits)
-
-  loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(logits=logits, labels=Y))
-  var_list = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope=scope)
-  train_op = optimizer.minimize(loss, var_list=var_list)
-        
-  correct_pred = tf.equal(tf.argmax(prediction, 1), tf.argmax(Y, 1))
+  loss = tf.keras.losses.CategoricalCrossentropy(from_logits=True)(Y, logits)
+  correct_pred = tf.equal(tf.argmax(prediction, 1), Y)
   accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
+  return loss, accuracy
 
-  return train_op, loss, accuracy
+def accuracy(dataset):
+  accs = []
 
-tf.reset_default_graph()
-if True:
-    # tf Graph input
-    X = tf.placeholder("float32", [timesteps, batch_size, 1])
-    Y = tf.placeholder("float32", [batch_size, num_classes])
-    
-    (lds_train_op, lds_loss, lds_accuracy), (lnlam, laminit) = lds(X, Y, "LDStack")
-    (gru_train_op, gru_loss, gru_accuracy), gru_layer = gru(X, Y, "GRU")
-    (lstm_train_op, lstm_loss, lstm_accuracy), lstm_layer = lstm(X, Y, "LSTM")
-    losses = [lds_loss, gru_loss, lstm_loss]
-    accuracies = [lds_accuracy, gru_accuracy, lstm_accuracy]
-
-    init = tf.global_variables_initializer()
-
-    #print("GRU parameters: ", gru_layer.count_params())
-    #print("LSTM parameters: ", lstm_layer.count_params())
-    print("LDStack parameters: ", num_parameters("LDStack"))
-
-def batched_accuracies(accuracies, data):
-  num_batches = 12
-  total_batches = int(np.floor(data.num_examples / batch_size))
-  rand_batches = np.random.choice(total_batches, num_batches, replace=False)
-  accs = np.zeros(shape=[num_batches, len(accuracies)])
-
-  for s,i in enumerate(rand_batches):
-    test_data = data.images[i*batch_size:(i+1)*batch_size].reshape((batch_size, timesteps, num_input))
-    test_data = np.transpose(test_data, (1,0,2))
-    test_label = data.labels[i*batch_size:(i+1)*batch_size]
-
-    accs[s] = sess.run(accuracies, feed_dict={X: test_data, Y: test_label})
+  for i, (batch_x, batch_y) in enumerate(dataset):
+    #lds_logits = lds(tf.complex(tf.transpose(batch_x, (1,0,2)), 0.0))[-1]
+    lds_logits = lds(batch_x)
+    _, accuracy = loss_accuracy(lds_logits, batch_y)
+    accs.append(accuracy)
   return np.mean(accs, axis=0)
               
+
 # Start training
-best_val_acc = 0.8
-sess = tf.Session()
+all_train_accs = np.zeros((num_epochs, 3))
+all_test_accs = np.zeros((num_epochs, 3))
 
-display_steps = round(training_steps / display_every) + 1
-all_train_accs = np.zeros((display_steps, 3))
-all_test_accs = np.zeros((display_steps, 3))
-display_step = 0
-if True:
-    # Run the initializer
-    sess.run(init)
+optimizer = tf.train.AdamOptimizer(learning_rate)
 
-    print(np.log(laminit))
-    print(sess.run(lnlam))
+#lds.compile(optimizer=optimizer, loss=tf.keras.losses.CategoricalCrossentropy(),
+#  metrics=[tf.keras.metrics.SparseCategoricalAccuracy()])
+#lds.fit(x_train, y_train, batch_size=batch_size, epochs=num_epochs, validation_data=(x_test, y_test))
 
-    for step in range(1, training_steps+1):
-        batch_x, batch_y = mnist.train.next_batch(batch_size)
-        batch_x = batch_x.reshape((batch_size, timesteps, num_input))
-        batch_x = np.transpose(batch_x, (1,0,2))
-        # Run optimization op (backprop)
-        sess.run(lds_train_op, feed_dict={X: batch_x, Y: batch_y})
-        sess.run(gru_train_op, feed_dict={X: batch_x, Y: batch_y})
-        sess.run(lstm_train_op, feed_dict={X: batch_x, Y: batch_y})
+def train_step(model, batch_x, batch_y, opt):
+  with tf.GradientTape() as tape:
+    #lds_logits = lds(tf.complex(tf.transpose(batch_x, (1,0,2)), 0.0))[-1]
+    logits = model(batch_x)
+    loss, accuracy = loss_accuracy(logits, batch_y)
+  #print(lds_loss.numpy(), lds_accuracy.numpy())
+  grads = tape.gradient(loss, model.trainable_variables)
+  grads, _ = tf.clip_by_global_norm(grads, 1.)
+  opt.apply_gradients(zip(grads, model.trainable_variables))
 
-        if step % display_every == 0 or step == 1:
-          all_train_accs[display_step] = batched_accuracies(accuracies, mnist.train)
-          all_test_accs[display_step]  = batched_accuracies(accuracies, mnist.test)
-          print(step, end=': ')
-          for i in range(3):
-            print(all_train_accs[display_step,i], all_test_accs[display_step,i], sep=' ', end= ' ')
-          print("")
-          display_step = display_step + 1
+for epoch in np.arange(num_epochs):
+  for step, (batch_x, batch_y) in enumerate(train_dataset):
+    train_step(lds, batch_x, batch_y, optimizer)
+  all_test_accs[epoch]  = accuracy(test_dataset)
+  print(epoch, end=': ')
+  print(all_test_accs[epoch])
+      
