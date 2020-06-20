Fixed TF2 gradient calculation and test

linear_recurrent_net/linear_recurrent_net/tensorflow_binding/__init__.py
@@ -17,7 +17,7 @@ def linear_recurrence(decays, impulses, initial_state=None):
         initial_state = tf.zeros_like(impulses[0, :])
 
     shape = tf.shape(decays)
-    rank = shape.get_shape()[0].value
+    rank = shape.get_shape()[0]
     if rank > 2:
         tail = tf.reduce_prod(shape[1:])
         decays = tf.reshape(decays, [shape[0], tail])
@@ -39,8 +39,8 @@ def _linear_recurrence_grad(op, dl_dresp):
     n_steps = tf.shape(impulses)[0]
 
     # forwards goes from h_0 to h_{T-1}
-    forwards_tail = linear_recurrence(tf.conj(decays), tf.conj(impulses), tf.conj(initial_state))[:-1, :]
-    forwards = tf.concat([tf.expand_dims(tf.conj(initial_state), 0), forwards_tail],
+    forwards_tail = linear_recurrence(tf.math.conj(decays), tf.math.conj(impulses), tf.math.conj(initial_state))[:-1, :]
+    forwards = tf.concat([tf.expand_dims(tf.math.conj(initial_state), 0), forwards_tail],
                          axis=0)
 
     reverse = lambda x: tf.reverse(x, axis=[0])
@@ -50,7 +50,7 @@ def _linear_recurrence_grad(op, dl_dresp):
     # output gradients from T-1, T-2, ..., 1
     dl_dh_head = reverse(
         linear_recurrence(
-            tf.conj(reverse(decays)[:-1, :]),
+            tf.math.conj(reverse(decays)[:-1, :]),
             reverse(dl_dresp)[1:, :],
             dl_dresp[-1, :],
         )
@@ -58,7 +58,7 @@ def _linear_recurrence_grad(op, dl_dresp):
 
     dl_dh = tf.concat([dl_dh_head, dl_dresp[-1:, :]], axis=0)
 
-    dl_dinit = tf.conj(decays[0, :]) * dl_dh[0, :]
+    dl_dinit = tf.math.conj(decays[0, :]) * dl_dh[0, :]
     dl_dimpulses = dl_dh
     dl_ddecays = dl_dh * forwards
 

linear_recurrent_net/linear_recurrent_net/tensorflow_binding/gradient_test.py
@@ -1,57 +1,33 @@
-from __future__ import print_function
-import numpy as np
-import tensorflow as tf
-import sys
-sys.path.append('./')
-from linear_recurrent_net.tensorflow_binding import linear_recurrence, linear_recurrence_cpu
-
 n_dims = 20
 n_steps = 30
 
 np.random.seed(2018)
-#decays = np.random.uniform(size=(n_steps, n_dims)).astype(np.complex64) 
-decays = (np.random.uniform(size=(n_steps, n_dims)) + 1.0j*np.random.uniform(size=(n_steps, n_dims))).astype(np.complex64) / 2.0
+decays = (np.random.uniform(size=(n_steps, n_dims)) + 1j*np.random.uniform(size=(n_steps, n_dims))).astype(np.complex64) / 2.0
 impulses = np.random.randn(n_steps, n_dims).astype(np.complex64)
 initial_state = np.random.randn(n_dims).astype(np.complex64)
 
-def run_test(linear_recurrence, sess):
-    inp = tf.constant(decays.real)
-    response = linear_recurrence(tf.complex(inp, tf.constant(decays.imag)), impulses, initial_state)
-    print('Decays.real grad err:',
-          tf.test.compute_gradient_error(inp, decays.shape,
-                                        response, impulses.shape)
-    )
-    inp = tf.constant(decays.imag)
-    response = linear_recurrence(tf.complex(tf.constant(decays.real), inp), impulses, initial_state)
-    print('Decays.imag grad err:',
-          tf.test.compute_gradient_error(inp, decays.shape,
-                                        response, impulses.shape)
-    )
+def err(vals):
+  for sym, num in zip(*vals):
+    return tf.linalg.norm(sym - num) / tf.linalg.norm(num)
 
-    inp = tf.constant(decays, dtype=tf.complex64)
-    response = linear_recurrence(inp, impulses, initial_state)
-    print('Decays grad err:',
-          tf.test.compute_gradient_error(inp, decays.shape,
-                                        response, impulses.shape)
-    )
+def run_test(lin_rec):
+  response = lambda inp: lin_rec(tf.complex(inp, decays.imag), impulses, initial_state)
+  print('Decays.real grad err:', err(tf.test.compute_gradient(response, [decays.real])))
 
-    inp = tf.constant(impulses, dtype=tf.complex64)
-    response = linear_recurrence(decays, inp, initial_state)
-    print('Impulses grad err:',
-          tf.test.compute_gradient_error(inp, impulses.shape,
-                                        response, impulses.shape)
-    )
+  response = lambda inp: lin_rec(tf.complex(decays.real, inp), impulses, initial_state)
+  print('Decays.imag grad err:', err(tf.test.compute_gradient(response, [decays.imag])))
 
-    inp = tf.constant(initial_state, dtype=tf.complex64)
-    response = linear_recurrence(decays, impulses, inp)
-    print('Initial state grad err:',
-          tf.test.compute_gradient_error(inp, initial_state.shape,
-                                        response, impulses.shape)
-    )
+  response = lambda inp: lin_rec(inp, impulses, initial_state)
+  print('Decays grad err:', err(tf.test.compute_gradient(response, [decays])))
+
+  response = lambda inp: lin_rec(decays, inp, initial_state)
+  print('Impulses grad err:', err(tf.test.compute_gradient(response, [impulses])))
+
+  response = lambda inp: lin_rec(decays, impulses, inp)
+  print('Initial state grad err:', err(tf.test.compute_gradient(response, [initial_state])))
 
 if __name__ == "__main__":
-  with tf.Session() as sess:
-    print("GPU")
-    run_test(linear_recurrence, sess)
-    print("CPU")
-    run_test(linear_recurrence_cpu, sess)
+  print("GPU")
+  run_test(linear_recurrence)
+  print("CPU")
+  run_test(linear_recurrence_cpu)